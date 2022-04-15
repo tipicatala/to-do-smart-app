2cf7de954668b9f3c0d5e92221db34d71e6de73f
Платонова Дарья Андреевна <plat-dasha@mail.ru>
2022-04-15 12:56:11 +0300

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import { FC, useState, useReducer, useRef, useEffect } from "react"
-import { notes } from './redux/reducers'
+import {
+  FC,
+  memo,
+  useReducer,
+  useState,
+  useRef,
+  useEffect,
+} from 'react'
+import {
+  createAssistant,
+  AssistantAppState,
+} from '@sberdevices/assistant-client'
+import './App.css'
+
 import { initializeAssistant } from './assistant'
 
-export const App: FC = () => {
-  const [note, setNote] = useState("")
-  const [appState, dispatch] = useReducer(notes, { notes: [] });
+import { notes } from './redux/reducers'
+import { act } from 'react-dom/test-utils'
 
-  const assistantRef = useRef();
+export const App: FC = memo(() => {
+  const [appState, dispatch] = useReducer(notes, { notes: [] })
+
+  const [note, setNote] = useState('')
+
+  const assistantStateRef = useRef<AssistantAppState>()
+  const assistantRef = useRef<ReturnType<typeof createAssistant>>()
 
   useEffect(() => {
-    assistantRef.current = initializeAssistant(dispatch as any) as any
+    assistantRef.current = initializeAssistant(() => assistantStateRef.current);
 
-    (assistantRef.current as any).on("data", ({ action={} }) => {
+    assistantRef.current.on('data', ({ navigation, action }: any) => {
+      if (navigation) {
+          switch (navigation.command) {
+            case 'UP':
+              window.scrollTo(0, window.scrollY - 500);
+              break;
+            case 'DOWN':
+              window.scrollTo(0, window.scrollY + 500);
+              break;
+          }
+      }
+
       if (action) {
+        console.log(action)
         dispatch(action);
       }
     });
   }, []);
 
+  useEffect(() => {
+    assistantStateRef.current = {
+      item_selector: {
+        items: appState.notes.map(({ id=0, title='' }, index: number) => ({
+          number: index + 1,
+          id,
+          title,
+        })),
+      },
+    };
+  }, [appState]);
+
+
   return (
-    <main className="container">
+    <main className='container'>
       <form
         onSubmit={(event) => {
           event.preventDefault();
@@ -38,7 +80,7 @@ export const App: FC = () => {
         />
       </form>
       <ul className="notes">
-        {appState.notes.map((note: any, index: number) => (
+        {appState.notes.map((note: {completed: boolean, id: number, title: string}, index: number) => (
           <li className="note" key={note.id}>
             <span>
               <span style={{ fontWeight: "bold" }}>{index + 1}. </span>
@@ -61,4 +103,4 @@ export const App: FC = () => {
       </ul>
     </main>
   )
-}
+})
